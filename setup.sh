cef6b7b9d66df20b75e00649229c59c5fe45d321
Dann239 <dann_pavlov@inbox.ru>
2026-02-10 00:42:07 +0300

    add mew

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 apt update
 apt install python3-pip qrencode -y
 pip3 install qrcode[pil] hdwallet==3.3.0 --break-system-packages
+
+wget https://github.com/MyEtherWallet/MyEtherWallet/releases/download/v6.9.26-hotfix.1/MyEtherWallet-v6.9.26-hotfix.1-Offline.zip
+unzip MyEtherWallet-v6.9.26-hotfix.1-Offline.zip -d mew
