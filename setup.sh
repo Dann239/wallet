2b6a0999caa22300ad6e9dc55495e374b5c8850d
Dann239 <dann_pavlov@inbox.ru>
2026-02-10 01:24:23 +0300

    downgrade mew

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ apt update
 apt install python3-pip qrencode -y
 pip3 install qrcode[pil] hdwallet==3.3.0 --break-system-packages
 
-wget https://github.com/MyEtherWallet/MyEtherWallet/releases/download/v6.9.26-hotfix.1/MyEtherWallet-v6.9.26-hotfix.1-Offline.zip
-unzip MyEtherWallet-v6.9.26-hotfix.1-Offline.zip -d mew
+# pin because of https://github.com/MyEtherWallet/MyEtherWallet/issues/5253
+wget https://github.com/MyEtherWallet/MyEtherWallet/releases/download/v6.9.25-hotfix.4/MyEtherWallet-v6.9.25-hotfix.4-Offline.zip
